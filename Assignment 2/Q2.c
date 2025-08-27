13bbb916667024ba6032ca68c5f9a30794a8449a
Lakshay Tyagi <lakshayindia420@gmail.com>
2025-08-27 20:10:31 +0000

    Q2

--- Assignment 2/Q2.c
+++ Assignment 2/Q2.c
@@ -7,4 +7,36 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <unistd.h>
+#include <semaphore.h>
+#define NUM_ITERATIONS 10000
 
+int counter = 0;
+sem_t sem;
+
+void* decrement_function(void* arg){
+    for (int i = 0; i < NUM_ITERATIONS; i++){
+        sem_wait(&sem);
+        counter++;
+        sem_post(&sem);
+    }
+    return NULL;
+}
+
+int main(){
+    pthread_t thread1;
+    pthread_t thread2;
+    if (sem_init(&sem, 0, 1) != 0){
+        printf("Failed to initialise the semaphore");
+        return 1;
+    }
+
+    pthread_create(&thread1, NULL, decrement_function, NULL);
+    pthread_create(&thread2, NULL, decrement_function, NULL);
+
+    pthread_join(thread1, NULL);
+    pthread_join(thread2, NULL);
+
+    sem_destroy(&sem);
+    printf("Final result : %d", counter);
+    return 0;
+}
